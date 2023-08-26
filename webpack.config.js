e30a9490a3d1142a16ad53356fd786560b676d97
florinnutiu <florin@stakelogic.com>
2023-08-26 18:19:12 +0200

    Fixed package version conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
-
+// HACK: OpenSSL 3 does not support md4 any more, but webpack hardcodes it all over the place: https://github.com/webpack/webpack/issues/13572
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm === "md4" ? "sha256" : algorithm);
 
 module.exports = {
     mode: "development",
@@ -21,8 +24,8 @@ module.exports = {
         ]
     },
     devServer: {
-        host:'localhost',
+        host: 'localhost',
         port: 8080
     },
-    performance: { hints: false }
+    performance: {hints: false}
 };
